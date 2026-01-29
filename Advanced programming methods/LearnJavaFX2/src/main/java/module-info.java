c898fa5f0296e14c9c11c37d3b20c0e4f998d547
Vlad Enache <ioan.vlad.enache@stud.ubbcluj.ro>
2026-01-29 17:52:39 +0100

    Reorganization
    Reorganization

--- Advanced programming methods/LearnJavaFX2/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.learnjavafx2 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.learnjavafx2 to javafx.fxml;
-    exports com.example.learnjavafx2;
-}
